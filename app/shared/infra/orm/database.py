1bd6b037b3152763b072f097a72bc47500185fc0
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2021-02-01 00:33:08 -0300

    Fix: Automatic create database

--- app/shared/infra/orm/database.py
+++ app/shared/infra/orm/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import create_database, database_exists
 from sqlalchemy.orm import sessionmaker
 
 
@@ -7,6 +8,9 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:docker@localhost:5432/t10_datab
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
+if not database_exists(engine.url):
+  create_database(engine.url)
+  
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
